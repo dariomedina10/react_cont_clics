0fb5fd243141797a661e08a5bb61dbd27a8e29bf
dariomedina10 <dariomedina10@gmail.com>
2025-01-29 02:18:40 -0400

    importacion de imagenes y creacion del componente boton

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import './App.css';
-import FreeCodecamplogo from './imagenes/freecodecamp.png';
+import FreeCodeCampLogo from './imagenes/freecodecamp.png';
 
 function App() {
   return (
@@ -9,11 +9,12 @@ function App() {
      <div className='freecodecamp-logo-contenedor'>
      <img
       className='freecodecamp-logo'
-      src={'FreeCodecamplogo'}
-      alt='logo de freecodecamp'
-      />
-      
-     </div>
+      src={FreeCodeCampLogo}
+      alt='logo de freecodecamp'/>
+       </div>
+       <div> className='contenedor-principal'
+       
+       </div>
     </div>
   );
 }
